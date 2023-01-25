2a52d1375fcaf9c0b74e0f0715e8d922f6501141
Joseph Ayman <josephayman@mail.com>
2023-01-25 17:37:02 +0100

    Oppgave 2 beslutninger

---
+++ Oblig 1/beslutninger.py
@@ -0,0 +1,10 @@
+x = input("Vil du ha en brus? (ja/nei)\n")
+  
+if x == "ja":
+    print("Her har du en brus!")
+elif x == "nei":
+    print("Den er grei.")
+else:
+    print("Det forstod jeg ikke helt.")
+
+
