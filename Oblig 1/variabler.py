7cb51593ebdbb6307f945395dd6ecbd16f7cad07
Joseph Ayman <josephayman@mail.com>
2023-01-25 17:36:37 +0100

    Oppgave 1 variabler

---
+++ Oblig 1/variabler.py
@@ -0,0 +1,21 @@
+print("Hei Student!")
+  
+navn = input ("Hva heter du?")
+
+print ("Hei " + navn)
+
+# oppgave 1 del 4
+
+v1 = 342
+v2 = 12
+
+print("Differansen er", v1 - v2) ## Differansen er 330
+
+# oppgave 1 del 6
+navn2 = input("Skriv ett nytt navn ")
+sammen = navn + " " + navn2
+print (sammen)
+
+# oppgave 1 del 7
+sammen = navn + " og " + navn2
+print(sammen)
