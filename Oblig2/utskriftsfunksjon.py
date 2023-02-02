feda4800946ee86e90cf882d873fe8b9b24317ce
Joseph Ayman <josephayman@mail.com>
2023-02-02 14:09:35 +0100

    Oppgave 1

---
+++ Oblig2/utskriftsfunksjon.py
@@ -0,0 +1,10 @@
+## Oppgave 1 del 2
+def utskrift():
+    navn = input("Skriv inn navn: ")
+    sted = input("Hvor kommer du fra? ")
+    print ("Hei, " + navn + "! Du er fra " + sted + ".")
+
+# Kall funksjonen 3 ganger
+utskrift()
+utskrift()
+utskrift()
