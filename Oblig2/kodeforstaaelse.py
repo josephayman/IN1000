de0bfeab980a512fae5d729d1908978b94eb3945
Joseph Ayman <josephayman@mail.com>
2023-02-02 14:10:22 +0100

    Oppgave 4

---
+++ Oblig2/kodeforstaaelse.py
@@ -0,0 +1,12 @@
+
+heltallSomTekst = input( "Tast inn et heltall! " )
+heltall = int( heltallSomTekst )
+if heltall < 10:
+    print ( heltall , "Hei!" )
+
+# Vil dette programmet kjøre?
+## Nei, fordi vi ikke kan legge sammen en tekststreng og et heltall
+
+# Hvilke problemer vil vi kunne møte på når vi kjører denne koden?
+## 1. Vi får en feilmelding fordi vi ikke kan legge sammen en tekststreng og et heltall
+## 2. Vi vil få ingenting  hvis vi skriver inn et tall som er større enn 10
