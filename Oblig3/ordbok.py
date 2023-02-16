c127bfaabcb62a10bcb151d08be9c348b4cbb274
Joseph Ayman <josephayman@mail.com>
2023-02-16 14:13:36 +0100

    Oppgave 2

---
+++ Oblig3/ordbok.py
@@ -0,0 +1,13 @@
+# Del 1
+varer = {
+    "melk": 14.90,
+    "brød": 24.90,
+    "yoghurt": 12.90,
+    "pizza": 39.90,
+}
+
+print(varer)
+
+# Del 2
+varer[input("Skriv inn en vare: ")] = float(input("Skriv inn pris: ")) # Validering med float for å kunne ha desimaler
+varer[input("Skriv inn en vare: ")] = float(input("Skriv inn pris: "))
